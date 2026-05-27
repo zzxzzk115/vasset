f7bbc6c58d274bc23cc15ebf4be46d90dd4ca58f
Lazy_V <954294627@qq.com>
2026-05-27 03:16:00 +0100

    fix: cook vpk example before build

--- examples/vpk/xmake.lua
+++ examples/vpk/xmake.lua
@@ -7,11 +7,22 @@ target("vasset-example-vpk")
     add_files("main.cpp")
 
     -- add deps
-    add_deps("vasset")
+    add_deps("vasset", "vasset-cli")
+
+    before_build(function (target)
+        local vasset_cli = target:dep("vasset-cli"):targetfile()
+        local asset_root = path.join(os.projectdir(), "resources")
+        local out_vpk = path.join(target:autogendir(), "out.vpk")
+
+        os.mkdir(target:autogendir())
+        target:data_set("vasset.example.vpk", out_vpk)
+        os.execv(vasset_cli, {"import", asset_root})
+        os.execv(vasset_cli, {"pack", asset_root, out_vpk})
+    end)
 
     -- copy out.vpk
 	after_build(function (target)
-		os.cp("$(scriptdir)/out.vpk", target:targetdir())
+		os.cp(target:data("vasset.example.vpk"), path.join(target:targetdir(), "out.vpk"))
 	end	)
 
     -- set target directory
